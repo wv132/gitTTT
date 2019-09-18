87a59b83ce75d1c14c19b123dc1e18af13cfca6c
wv132 <wouter-v@hotmail.com>
2019-09-18 08:04:14 +0200

    ...

--- src/main/java/com/novi/minigames/Field.java
+++ src/main/java/com/novi/minigames/Field.java
@@ -12,49 +12,7 @@ import java.util.*;
 public class Field {
     //Create fields (board)
     //char[] board;
-    int x;
-    int y;
     
-    
-    
-    
-        
-   
-   public void createBoard(char[] board) {
-       
-       board = new char[x*y];  
-        for (int i = 0; i < board.length; i++) {
-            board[i] = (char)('1'+i);
-        }
-        
-    }    
-
-    public void printBoard(char[] board) {
-        
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.print(board[i * 3 + j]);
-            }
-            System.out.println();
-        }
-             
-    }
-    public boolean boardFull(char[] board){
-        boolean isFull = true;
-        
-        for(int i = 0;i<board.length;i++){
-            if(board[i]!= 'X' || board[i] != 'O'){
-                isFull = false;
-            }
-           
-        }
-        return isFull;
-    }
-    
-    public int getBoardPosition(int position,char[] board){
-        return board[position];
-    }
-   
    
     
    
